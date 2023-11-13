3216059d210457e74800d2183c49cdce669f5cbd
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-11-12 22:09:51 -0300

    add final inputs and outputs to the main function

--- Lecture1/population.c
+++ Lecture1/population.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <cs50.h>
+
+int get_population_years(int start_population, int end_population);
 
 int main(void)
 {
-    printf("hello, world");
+    int start_population;
+    int end_population;
+
+    do
+    {
+        start_population = get_int("Start size: ");
+    }
+    while (start_population < 9);
+
+    do
+    {
+        end_population = get_int("End size: ");
+    } while (end_population < start_population);
+
+    int years = get_population_years(start_population, end_population);
+    printf("Years: %i\n", years);
+    
 }
 
+int get_population_years(int start_population, int end_population)
+{
+    int years = 0;
+    while (start_population < end_population)
+    {
+        start_population = start_population + (start_population / 3) - (start_population / 4);
+        years++;
+    }
+    return years;
+}
