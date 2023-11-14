f677784aa78c6b0e2433a55f0ab506e228e0e3f0
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-11-14 16:54:12 -0300

    fix spaces before #

--- Lecture1/mario/mario.c
+++ Lecture1/mario/mario.c
@@ -9,27 +9,27 @@ int main(void)
     do
     {
         height = get_int("Height: ");
-        print_pyramid(height);
-    } while (height < 1);
+    } while (height < 1 || height > 9);
+    print_pyramid(height);
 }
 
 void print_pyramid(int height)
 {
     for (int row = 0; row < height; row++)
     {
-        for (int space = 0; space < height - row; space++)
+        for (int space = 0; space < height - row - 1; space++)
         {
             printf(" ");
         }
 
-        for (int i = 0; i < row + 1; i++)
+        for (int brick = 0; brick < row + 1; brick++)
         {
             printf("#");       
         }
 
         printf(" ");
 
-        for (int i = 0; i < row + 1; i++)
+        for (int brick = 0; brick < row + 1; brick++)
         {
             printf("#");       
         }
