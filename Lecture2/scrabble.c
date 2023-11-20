e1cf3f787cf05cedfede75f0ecff4c33304e3f96
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-11-19 22:09:09 -0300

    fix compiling errors

--- Lecture2/scrabble.c
+++ Lecture2/scrabble.c
@@ -19,9 +19,9 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    if (score1 > score2){printf("Player 1 wins!\n")}
-    if (score1 < score2){printf("Player 2 wins!\n")}
-    if (score1 == score2){printf("Tie!\n")}
+    if (score1 > score2){printf("Player 1 wins!\n");}
+    if (score1 < score2){printf("Player 2 wins!\n");}
+    if (score1 == score2){printf("Tie!\n");}
 }
 
 int compute_score(string word)
