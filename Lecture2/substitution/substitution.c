4f1708ce6e104270e216c8d6ec251af643756172
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-11-23 16:13:13 -0300

    add encrypt_cipher function draft

--- Lecture2/substitution/substitution.c
+++ Lecture2/substitution/substitution.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+
+string encrypt_cipher(string key);
 
 int main(int argc, string argv[])
 {
-    return 0
+    string test = encrypt_cipher("aaaaaa");
+    return 0;
+}
+
+string encrypt_cipher(string key)
+{
+    string upper_key;
+    for (int character = 0; key[character] != '\0'; character++)
+    {
+        upper_key[character] = toupper(key[character]);
+    }
+    return upper_key;
 }
