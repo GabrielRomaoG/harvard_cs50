f69b0fdb9790f4ae046d91376f13eb507358d70a
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-12-25 20:49:04 -0300

    complete colorize function

--- lecture4/smiley/helpers.c
+++ lecture4/smiley/helpers.c
@@ -2,5 +2,14 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    for (int h_id = 0; h_id < height; h_id++)
+    {
+        for (int w_id = 0; w_id < width; w_id++)
+        {
+            if (image[h_id][w_id].rgbtRed == 0x00 && image[h_id][w_id].rgbtGreen == 0x00 && image[h_id][w_id].rgbtBlue == 0x00)
+            {
+                image[h_id][w_id].rgbtBlue = 0xff;
+            }
+        }
+    }
 }
