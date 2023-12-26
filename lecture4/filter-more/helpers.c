f614655b5345834cb9e67012ea6252dba6437a9c
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-12-26 15:11:14 -0300

    complete grayscale function

--- lecture4/filter-more/helpers.c
+++ lecture4/filter-more/helpers.c
@@ -1,8 +1,19 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h_id = 0; h_id < height; h_id++)
+    {
+        for (int w_id = 0; w_id < width; w_id++)
+        {
+            int rgb_average = round((image[h_id][w_id].rgbtRed + image[h_id][w_id].rgbtGreen + image[h_id][w_id].rgbtBlue) / 3.0);
+            image[h_id][w_id].rgbtRed = rgb_average;
+            image[h_id][w_id].rgbtGreen = rgb_average;
+            image[h_id][w_id].rgbtBlue = rgb_average;
+        }
+    }
     return;
 }
 
