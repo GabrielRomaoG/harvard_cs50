636e1ac4fa6badd34b6ad330b53acded7d25d357
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-12-26 15:39:48 -0300

    complete reflect function

--- lecture4/filter-more/helpers.c
+++ lecture4/filter-more/helpers.c
@@ -20,6 +20,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int w_id = 0; w_id < width / 2; w_id++)
+    {
+        for(int h_id = 0; h_id < height; h_id++)
+        {
+            RGBTRIPLE tmp_pixel = image[h_id][w_id];
+            int rev_pixel_id = width - w_id - 1;
+            image[h_id][w_id] = image[h_id][rev_pixel_id];
+            image[h_id][rev_pixel_id] = tmp_pixel;
+        }
+    }
     return;
 }
 
