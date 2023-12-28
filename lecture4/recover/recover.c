d378d81a90852ae0061fffd5aa90522ddf3deba1
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-12-28 13:53:31 -0300

    add input validation and file opening logic in main function

--- lecture4/recover/recover.c
+++ lecture4/recover/recover.c
@@ -3,5 +3,16 @@
  
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: recover file_path\n");
+        return 1;
+    }
+    FILE *rawFile = fopen(argv[1], "r");
+
+    if (rawFile == NULL)
+    {
+        printf("The programm couldn't open the file, please check the file path.");
+        return 1;
+    }
 }
