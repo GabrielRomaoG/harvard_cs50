49a94b933e7bdb99e1f47fcbade97895502d2141
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-12-31 14:42:54 -0300

    solve recover problem

--- lecture4/recover/recover.c
+++ lecture4/recover/recover.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdbool.h>
+
+#define BLOCK_SIZE 512
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -15,4 +18,37 @@ int main(int argc, char *argv[])
         printf("The programm couldn't open the file, please check the file path.");
         return 1;
     }
+
+    unsigned char buffer[BLOCK_SIZE];
+    int fileId = 0;
+    FILE *imageFile = NULL;
+    char fileName[8];
+
+    while (fread(buffer, 1, BLOCK_SIZE, rawFile) == BLOCK_SIZE)
+    {
+        bool isJpgStart =  buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+        if (isJpgStart)
+        {
+            if (fileId > 0)
+            {
+                fclose(imageFile);
+            }
+            sprintf(fileName, "%03i.jpg", fileId);
+
+            imageFile = fopen(fileName, "w");
+
+            fwrite(buffer, 1, BLOCK_SIZE, imageFile);
+
+            fileId++;
+        }
+        else if (!isJpgStart && fileId > 0)
+        {
+            fwrite(buffer, 1, BLOCK_SIZE, imageFile);
+        }
+    }
+    fclose(imageFile);
+    fclose(rawFile);
+
+    return 0;
 }
+
